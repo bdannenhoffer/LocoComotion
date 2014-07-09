acb282e2aeb47675cd82b595d203089caf65bbdc
Brandon Dannenhoffer <brandondannenhoffer@Brandon-Dannenhoffers-MacBook-Pro.local>
2014-07-09 12:41:04 -0400

    Connected main menu and level selection buttons

---
+++ brandondannenhoffer.spritebuilder/Source/LevelSelectionScene.h
@@ -0,0 +1,13 @@
+//
+//  LevelSelectionScene.h
+//  brandondannenhoffer
+//
+//  Created by Brandon Dannenhoffer on 7/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface LevelSelectionScene : CCScene
+
+@end
---
+++ brandondannenhoffer.spritebuilder/Source/LevelSelectionScene.m
@@ -0,0 +1,13 @@
+//
+//  LevelSelectionScene.m
+//  brandondannenhoffer
+//
+//  Created by Brandon Dannenhoffer on 7/9/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "LevelSelectionScene.h"
+
+@implementation LevelSelectionScene
+
+@end
